cb980c634eff2667e8ab42359713840af89db57d
moray95 <moraybaruh@me.com>
2014-09-29 21:35:24 +0200

    Tableau a 2 dimension, convertion de tableau en data faite.

--- OCR/OCR/image_handle.c
+++ OCR/OCR/image_handle.c
@@ -20,7 +20,7 @@ typedef struct pixel
     unsigned char blue;
 }pixel;
 // Loads image from url
-int imageFromFile(const char file[], int *width, int *heigth)
+int imageFromFile(const char file[], int *width, int *heigth) // Thibault style
 {
     int n;
     imagePtr *data = stbi_load(file,width,heigth,&n,3);
@@ -74,3 +74,53 @@ int imageFromFile(const char file[], int *width, int *heigth)
     stbi_image_free(data);
     return 1;
 }
+
+int imageFromFile2(const char file[], int *width, int *heigth) // Moray style
+{
+    int n;
+    imagePtr *data = stbi_load(file,width,heigth,&n,3);
+    struct pixel image[*width][*heigth];
+    if(data == NULL)
+    {
+        printf("Failure");
+        return 0;
+    }
+
+
+    int x, y;
+    int i,j;
+    printf("%u\n",n);
+
+    for(i = 0; i < (*width)*(*heigth)*3; i+=3)
+    {
+        image[i % *width][i/ *width].red = data[i];
+        image[i % *width][i/ *width].green = data[i+1];
+        image[i % *width][i/ *width].blue = data[i+2];
+    }
+    /*for(i = 0; i < (*width)*(*heigth); i++)
+    {
+        printf("%i - {%i,%i,%i}\n", i, image[i].red,image[i].green,image[i].blue);
+    }*/
+
+    printf("success - %i",(*width)*(*heigth));
+    char chemin[] = "test.bmp";
+    stbi_write_bmp(&chemin[0], *width, *heigth, 3, imageToData(*width, *heigth, image));
+    stbi_image_free(data);
+    return 1;
+}
+
+
+imagePtr * imageToData(int width, int heigth, struct pixel image[width][heigth])
+{
+    imagePtr *data = malloc(sizeof(unsigned char)*width*heigth*3);
+
+    int i;
+    for(i = 0; i < width*heigth*3; i+=3)
+    {
+            data[i]     = image[i%width][i/width].red;
+            data[i + 1] = image[i%width][i/width].green;
+            data[i + 2] = image[i%width][i/width].blue;
+    }
+    return data;
+}
+
