52adb35eea5aa720e3c15184f9083a4770c6f5e9
moray95 <user@Morays-MacBook-Pro.local>
2014-10-05 13:09:06 +0200

    Load returns image
    Load now returns the image instead of opening it.

--- OCR/image_handle.h
+++ OCR/image_handle.h
@@ -1,1 +1,2 @@
-int load(int argc, char* argv[]);
+#include "opencv/cv.h"
+IplImage* load(int argc, char* argv[]);
--- OCR/main.c
+++ OCR/main.c
@@ -6,6 +6,21 @@
 
 int main (int argc, char* argv[])
 {
-  load(argc, argv);
+  IplImage *img = load(argc, argv);
+    if(img != NULL)
+    {
+    const char* window_title = "Perfect Image";
+    cvNamedWindow (window_title, CV_WINDOW_AUTOSIZE);
+    cvShowImage (window_title, img);
+    cvWaitKey(0);
+    char path[] ="result.bmp";
+    cvSaveImage(&path[0], img, NULL);
+    cvDestroyAllWindows();
+    cvReleaseImage(&img);
+    }
+    else
+    {
+        printf("Error loading image.\n");
+    }
   return EXIT_SUCCESS;
 }
