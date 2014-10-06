1e4f880169a04665b4b0dc9e818607d5f8432e05
moray95 <user@Morays-MacBook-Pro.local>
2014-10-07 00:42:11 +0200

    Image Comparison
    Added compare function to compare images. Generalised load function.

--- OCR/image_handle.c
+++ OCR/image_handle.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <opencv/highgui.h>
-#include <opencv/cv.h>
+#include "opencv/highgui.h"
+#include "opencv/cv.h"
 
-IplImage* load(int argc, char* argv[])
+IplImage* load(char* image)
 {
   IplImage* img = NULL; 
   
   int x,y;
 
-  if (argc < 2)
-  {
-    //fprintf (stderr, "usage: %s IMAGE\n", argv[0]);
-    return NULL;
-  }
-  img = cvLoadImage(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+  img = cvLoadImage(image, CV_LOAD_IMAGE_GRAYSCALE);
 
   if (img == NULL)
   {
@@ -23,7 +18,6 @@ IplImage* load(int argc, char* argv[])
   }
   int colorBackground = CV_IMAGE_ELEM(img, uchar, 0, (0 * 3));
   int i = img->nChannels;
-  printf("%d\n",i);
   for (y = 0; y < img->height;y++)
   {
     for (x = 0; x < img->width;x++)
@@ -42,3 +36,36 @@ IplImage* load(int argc, char* argv[])
     return img;
 
 }
+
+double compare(IplImage *img1, IplImage *img2)
+{
+    if (img1 == NULL || img2 == NULL) {
+        return -1;
+    }
+    CvSize size;
+    size.width = img1->width;
+    size.height = img1->height;
+    IplImage *newImage = cvCreateImage(size, 8, 1);
+    if (img1->width != img2->width || img1->height != img2->height) {
+        cvResize(img2, newImage, CV_INTER_LINEAR);
+    }else{
+        newImage = img2;
+    }
+    unsigned long long n = 0;
+    for(int i = 0; i < img1->width; i++)
+    {
+        for(int j = 0; j < img1->height; j++)
+        {
+            if (CV_IMAGE_ELEM(img1, uchar, j, i) == CV_IMAGE_ELEM(newImage, uchar, j, i))
+            {
+                n++;
+            }
+        }
+    }
+    return ((double) n/(img1->width * img1->height))*100;
+}
+
+
+
+
+
--- OCR/main.c
+++ OCR/main.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "opencv/highgui.h"
-#include "opencv/cv.h"
+#include <opencv/highgui.h>
+#include <opencv/cv.h>
 #include "image_handle.h"
+#include <string.h>
 
 int main (int argc, char* argv[])
 {
-  IplImage *img = load(argc, argv);
+    int i = 0;
+
+    IplImage *img = load(argv[1]);
     if(img != NULL)
     {
-    const char* window_title = "Perfect Image";
-    cvNamedWindow (window_title, CV_WINDOW_AUTOSIZE);
-    cvShowImage (window_title, img);
-    cvWaitKey(0);
-    char path[] ="result.bmp";
-    cvSaveImage(&path[0], img, NULL);
-    cvDestroyAllWindows();
-    cvReleaseImage(&img);
+        const char* window_title = "Perfect Image";
+        cvNamedWindow (window_title, CV_WINDOW_AUTOSIZE);
+        cvShowImage (window_title, img);
+        cvWaitKey(0);
+        char path[] = "result.bmp";
+        cvSaveImage(&path[0], img, NULL);
+        cvDestroyAllWindows();
+        cvReleaseImage(&img);
     }
     else
     {
