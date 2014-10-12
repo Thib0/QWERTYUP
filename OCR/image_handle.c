bda7e9e76c409ddd077c65d2a02f45ed464cfcb1
jeajeamax <jeanne.petit@epita.fr>
2014-10-12 12:21:18 +0200

    Detection des lignes

--- OCR/image_handle.c
+++ OCR/image_handle.c
@@ -3,6 +3,10 @@
 #include "opencv/highgui.h"
 #include "opencv/cv.h"
 #include "image_handle.h"
+int detect_line(IplImage *img, int *lines_number );
+int line_value (IplImage *img,int y);
+int color_line(IplImage *img, int y);
+
 
 IplImage* load(char* image)
 {
@@ -74,6 +78,14 @@ IplImage* load(char* image)
                        }
                 }
         }
+
+    
+    // call fonction to detect lines                                                                                                            
+    int *lines_number = malloc(1000);
+    int size_lines_number = detect_line(img,lines_number);
+    if (size_lines_number != 0)
+      printf("succes");
+
     return img;
 
 }
@@ -106,3 +118,58 @@ double compare(IplImage *img1, IplImage *img2)
     return ((double) n/(img1->width * img1->height))*100;
 }
 
+int detect_line(IplImage *img, int *lines_number )
+{
+
+  int y,y0,y1,y2,size_lines_number = 0;
+  for (y=1; y<( img->height - 1);y++)
+    {
+      y0 = line_value(img,y-1); // test y-1                                                                                                       
+      y1 = line_value(img,y);   // test y                                                                                                         
+      y2 = line_value(img,y+ 1); // test y+1                                                                                                      
+
+      // printf("%i\n",y);                                                                                                                        
+
+      if ((y1==0 && y2==1) || (y1==0 && y0==1))
+	{
+
+	  lines_number[size_lines_number] = y;
+	  size_lines_number ++;
+	}
+    }
+
+  for (int i = 0 ; i < size_lines_number;i++)
+    {
+      color_line(img,lines_number[i]);
+    }
+
+  return size_lines_number;
+}
+
+int line_value (IplImage *img,int y)
+{
+  int x;
+
+  for (x = 0 ; x < img->width; x++)
+    {
+      // if black pixel return 1                                                                                                                  
+      if (CV_IMAGE_ELEM(img,uchar,y,x) == 0)
+	{
+	  return 1;
+	}
+    }
+  // if only white pixel, return 0                                                                                                              
+  return 0;
+}
+
+int color_line (IplImage *img, int y)
+{
+  int x;
+  for (x = 0 ; x < img->width; x++)
+    {
+
+      CV_IMAGE_ELEM(img,uchar,y,x)=0;
+    }
+
+  return 0;
+}
