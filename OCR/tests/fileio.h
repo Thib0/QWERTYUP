9291404dfd8840402c7eb9279336c8babb115a09
Moray Baruh <moraybaruh@me.com>
2014-12-06 14:24:03 +0100

    File save and get string
    update makefile

---
+++ OCR/tests/fileio.h
@@ -0,0 +1,13 @@
+#ifndef _FILE_IO_
+#define _FILE_IO_
+#include "image_detection.h"
+#include "char_treatment.h"
+#include "opencv/cv.h"
+#include "opencv/highgui.h"
+#include "neural_network.h"
+
+char *getString(struct rect_char *rect_chars, size_t size, IplImage *img, neural_network *network);
+
+int saveToFile(char *string, char *fileName);
+
+#endif
